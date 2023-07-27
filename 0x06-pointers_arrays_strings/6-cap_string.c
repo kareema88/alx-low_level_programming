43761181a8b744474f6c9c5aa8ff1a6fb1c917bf
kareema88 <dfkarima88@gmail.com>
2023-07-27 13:09:50 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,24 @@
  */
 char *cap_string(char *s)
 {
-	int k, d;
+    int count = 0, k;
+    int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	char index[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
-	'"', '(', ')', '{', '}'};
-
-	for (k = 0; s[k] != '\0'; k++)
-	{
-
-		if (k == 0 && s[k] >= 'a' && s[k] <= 'z')
-		{
-		s[k] -= 32;
-
-	for (d = 0; d < 13 ; d++)
-
-		if (s[k] == index[d])
-		{
-			if (s[k + 1] >= 'a' && s[k + 1] <= 'z')
-			{
-			s[k + 1] -= 32;
-			}
-
-		}
-		}
-
-	}
-	return (s);
+    if (*(s + count) >= 97 && *(s + count) <= 122)
+        *(s + count) = *(s + count) - 32;
+    count++;
+    while (*(s + count) != '\0')
+    {
+        for (k = 0; k < 13; k++)
+        {
+            if (*(s + count) == sep_words[k])
+            {
+                if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+                    *(s + (count + 1)) = *(s + (count + 1)) - 32;
+                break;
+            }
+        }
+        count++;
+    }
+    return (s);
 }
