092bf8f1141d1e840ed163171dfe06f9c6cea2ed
kareema88 <dfkarima88@gmail.com>
2023-08-10 02:19:59 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,12 +10,35 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-	void *p
+	unsigned int k, d, len1 = 0, len2 = 0;
+	char *str;
 
-	p = malloc(b);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (p == NULL)
-	exit (98);
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-	return (p);
+	if (n > len2)
+		n = len2;
+
+	str = malloc(sizeof(char) * (len1 + n + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (k = 0; s1[k] != '\0'; k++)
+		str[k] = s1[k];
+
+	for (d = 0; d < n; d++)
+	{
+		str[k] = s2[d];
+		k++;
+	}
+	str[k] = '\0';
+
+		return (str);
 }
