49443d288c387c993dcbe052cb2378e04a12eafb
kareema88 <dfkarima88@gmail.com>
2023-08-10 09:00:54 -0700

    task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,11 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		p = malloc(new_size);
-
-		if (p == NULL)
-			return (NULL);
-		return (p);
+		return (malloc(new_size));
 	}
 	if (new_size == old_size)
 		return (ptr);
