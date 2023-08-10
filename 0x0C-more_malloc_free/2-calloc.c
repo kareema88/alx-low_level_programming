e821237c4882267eb2a138fe84510f02e851be4d
kareema88 <dfkarima88@gmail.com>
2023-08-10 06:16:04 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: string
+ * @b: charachter
+ * @n: integer
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int k;
+
+	for (k = 0; n > 0; k++)
+	{
+		s[k] = b;
+		n--;
+	}
+	return (s);
+}
+/**
+ * *_calloc - function that allocates memory for an array
+ * @nmemb: nembre of members
+ * @size: size
+ *
+ * Return: NULL or pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pt;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pt = malloc(sizeof(int) * nmemb);
+
+	if (pt == 0)
+		return (NULL);
+
+	_memset(pt, 0, sizeof(int) * nmemb);
+	return (pt);
+}
