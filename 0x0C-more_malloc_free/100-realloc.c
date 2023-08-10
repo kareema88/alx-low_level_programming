17e600958d39fdfc78bc8451c37e83427f97b106
kareema88 <dfkarima88@gmail.com>
2023-08-10 08:35:50 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function that copies memory area
+ * @dest: destination
+ * @src: charachter
+ * @n: integer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		dest[k] = src[k];
+
+	}
+	return (dest);
+}
+
+/**
+ * *_realloc - reallocate a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes of the new memory block
+ * Return: NULL or pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (0);
+	}
+
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+
+	if (p == NULL)
+		return (NULL);
+
+	if (new_size > old_size)
+		_memcpy(p, ptr, old_size);
+	if (new_size < old_size)
+		_memcpy(p, ptr, new_size);
+
+	free(ptr);
+	return (p);
+}
